c1dcce1a79413600e0d658ba8475889b49f705bc
Henry <doubleh777@nhnnext.org>
2014-06-01 22:38:21 +0900

    C homework

---
+++ MyFunction.h
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct member{
+	int ID;
+	char Name[30];
+	char Adress[100];
+	char Phone[30];
+}typedef Member;
+
+void printPersonalInformation(Member*);
+void initID(Member*);
+void addMember(Member*);
+void dataInput(Member*);
+void mainPage(Member *);
+void printPersonalInformation(Member *);
+void searchMember(Member *);
+void errorPhoneNumber(Member *, int);
+void editMember(Member*, int);
+void editIDNumber(Member*, int, char*);
+void editName(Member*, int, char*);
+void editAdress(Member*, int, char*);
+void editPhoneNumber(Member*, int, char*);
+void editIDNumber_errorCase(Member*, int , char*);
+void editPhoneNubmer_errorCase(Member*, int, char*, char*);
+void deleteMmeber(Member*);
+void searchWithID(Member*);
+void searchWithName(Member*);
+void saveMember(Member*);
+void _Exit(Member*, Member*);
+void realloction(Member*);
